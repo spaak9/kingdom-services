f3d4990993fde5a501c9f72a7a5abf7cbc85a2c2
spaak9 <aass1aasss1@hotmail.com>
2026-09-04 09:14:21 +0300

    Add services array and isServiceSlug function

--- data/services.ts
+++ data/services.ts
@@ -1,1 +1,10 @@
+export const services = [
+  'plumber',
+  'electrician',
+  'painting',
+  'tiling'
+];
 
+export function isServiceSlug(slug: string): boolean {
+  return services.includes(slug);
+}
